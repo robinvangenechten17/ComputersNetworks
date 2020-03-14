d0603555ea4afdf76ca31ed576fb411937b6af40
robinvangenechten17 <robin.vangenechten@student.kuleuven.be>
2020-03-14 15:15:49 +0100

    Update

--- exercicesSession1/HttpURLConnectionExample.java
+++ exercicesSession1/HttpURLConnectionExample.java
@@ -13,6 +13,11 @@ import java.net.Socket;
 import java.net.SocketException;
 import java.net.SocketTimeoutException;
 import java.net.URL;
+import java.io.File;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
 /**
  * @author Robin Van Genechten en Quentin
  */
@@ -24,23 +29,23 @@ public class HttpURLConnectionExample {
   HttpURLConnectionExample http = new HttpURLConnectionExample();
  
      // Sending get request
- // http.sendingGetRequest();
+  http.sendingGetRequest();
   
     // Sending post request
-  http.sendingPostRequest();
+  //http.sendingPostRequest();
  
  }
  
  // HTTP GET request
  private void sendingGetRequest() throws Exception {
  
-  String url = "www.example.com"; 
+  String url = "www.google.com"; 
   // HttpURLConnection con = (HttpURLConnection) url.openConnection();
   Socket s=new Socket(url,80);
   //// By default it is GET request
   //con.setRequestMethod("GET");
   DataOutputStream dout=new DataOutputStream(new DataOutputStream(s.getOutputStream()));  
-  dout.writeBytes("GET http://www.example.com  HTTP/1.1 \n\n");
+  dout.writeBytes("GET /  HTTP/1.1 \n\n");
 	      
  // dout.writeBytes("GET / HTTP/1.1\n");
  // dout.writeBytes("Host: wlab.cs.bilkent.edu.tr:80\n\n"); 
@@ -53,24 +58,31 @@ public class HttpURLConnectionExample {
 //  System.out.println("Response code : "+ responseCode);
  
   // Reading response from input Stream
+  StringBuffer response = new StringBuffer();
   BufferedReader in = new BufferedReader(
           new InputStreamReader(s.getInputStream()));
-  String output;
-  StringBuffer response = new StringBuffer();
-	  while ((output = in.readLine())!=null) {
-		  System.out.println(output);
-		  response.append(output);
-		}
-  System.out.println("out while");
+  try { 
+	  s.setSoTimeout(5000);
+	  String output;
+		  while ((output = in.readLine())!=null) {
+			//  System.out.println(output);
+			  response.append(output);
+			}
+  }
+  catch (Exception e) {
+	  System.out.println("Time Out");
+  }
   //printing result from response
   System.out.println("result:");
   System.out.println(response.toString());
   String str = response.toString();
-  FileWriter myWriter = new FileWriter("/Users/robin/eclipse-workspace/Computer Networks/src/result");
+  FileWriter myWriter = new FileWriter("/Users/robin/eclipse-workspace/Computer Networks/src/" + url + ".HTML");
   
-  //myWriter.write("Files in Java might be tricky, but it is fun enough!");
+  //schrijft heel de website naar een file
   myWriter.write(str);
   myWriter.close();
+  //gaat opzoek naar image
+  
   in.close();
   dout.close();  
 	s.close();  
@@ -78,8 +90,20 @@ public class HttpURLConnectionExample {
 	System.out.println("GET IS DONE");
 }
 
- 
- 
+ private void findingimage(String fileName) throws Exception {
+	 File input = new File(fileName);
+	 Document doc = Jsoup.parse(input,"UTF-8");
+	 Elements img = doc.getElementsByTag("img");
+	 for (Element image : img) {
+		 String src = image.absUrl("src");
+		 System.out.println("Image found");
+		 System.out.println("src is;" + src);
+		 getImage(src);
+	 }
+ }
+ private void getImage(String src) {
+	 
+ }
  // HTTP Post request
  private void sendingPostRequest() throws Exception {
  
@@ -121,12 +145,13 @@ public class HttpURLConnectionExample {
    response.append(output);
   }
   System.out.println(response.toString());
+  System.out.println("closing");
   in.close();
   s.close();
   dout.close();  
   System.out.println("closed");
   //printing result from response
   
-  System.out.println("GET IS DONE");
+  System.out.println("POST IS DONE");
  }
 }
