fb20b5ba322469918039b8944449ec32f148dd15
robinvangenechten17 <robin.vangenechten@student.kuleuven.be>
2020-03-14 22:26:28 +0100

    Image

--- exercicesSession1/HttpURLConnectionExample.java
+++ exercicesSession1/HttpURLConnectionExample.java
@@ -16,6 +16,9 @@ import java.net.SocketException;
 import java.net.SocketTimeoutException;
 import java.net.URL;
 import java.io.File;
+import java.io.FileOutputStream;
+
+
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
@@ -32,7 +35,7 @@ public class HttpURLConnectionExample {
  
      // Sending get request
   http.sendingGetRequest("www.google.com");
-  
+  //http.sendingGetRequestforImage("www.google.com", "/images/branding/googlelogo/2x/googlelogo_color_92x30dp.png");
     // Sending post request
   //http.sendingPostRequest();
  
@@ -47,7 +50,9 @@ public class HttpURLConnectionExample {
   //// By default it is GET request
   //con.setRequestMethod("GET");
   DataOutputStream dout=new DataOutputStream(new DataOutputStream(s.getOutputStream()));  
-  dout.writeBytes("GET /images/branding/googlelogo/2x/googlelogo_color_272x92dp.png  HTTP/1.1 \n\n");
+  dout.writeBytes("GET /  HTTP/1.1 \r\n");
+  dout.writeBytes("Host: " + url +"\r\n"); 
+  dout.writeBytes("\r\n");// add an emptyline
 	      
  // dout.writeBytes("GET / HTTP/1.1\n");
  // dout.writeBytes("Host: wlab.cs.bilkent.edu.tr:80\n\n"); 
@@ -86,6 +91,7 @@ public class HttpURLConnectionExample {
   System.out.println("result:");
   System.out.println(response.toString());
   String str = response.toString();
+  
   FileWriter myWriter = new FileWriter("/Users/robin/eclipse-workspace/Computer Networks/src/" + url + ".HTML");
   
   //schrijft heel de website naar een file
@@ -97,17 +103,20 @@ public class HttpURLConnectionExample {
   dout.close();  
 	s.close();  
 	System.out.println("closed");
-	System.out.println("GET IS DONE");
+	System.out.println("GET Webpage IS DONE");
+	System.out.println("retreiving images");
+	
+	findingimage("/Users/robin/eclipse-workspace/Computer Networks/src/" + url + ".HTML",url);
 }
- private void sendingGetRequestforImage(String page) throws Exception {
+ private void sendingGetRequestforImage(String server, String imagelocation) throws Exception {
 	 
-	  String url = page; 
+	  String url = server; 
 	  // HttpURLConnection con = (HttpURLConnection) url.openConnection();
 	  Socket s=new Socket(url,80);
 	  //// By default it is GET request
 	  //con.setRequestMethod("GET");
 	  DataOutputStream dout=new DataOutputStream(new DataOutputStream(s.getOutputStream()));  
-	  dout.writeBytes("GET /images/branding/googlelogo/2x/googlelogo_color_272x92dp.png  HTTP/1.1 \n\n");
+	  dout.writeBytes("GET " +imagelocation +"  HTTP/1.1 \n\n");
 		      
 	 // dout.writeBytes("GET / HTTP/1.1\n");
 	 // dout.writeBytes("Host: wlab.cs.bilkent.edu.tr:80\n\n"); 
@@ -119,61 +128,63 @@ public class HttpURLConnectionExample {
 	  System.out.println("Sending get request : "+ url);
 	//  System.out.println("Response code : "+ responseCode);
 	 
-	  // Reading response from input Stream
-	  StringBuffer response = new StringBuffer();
-	  //BufferedInputStream in = new BufferedInputStream(s.getInputStream())
-	  BufferedReader in = new BufferedReader(
-	          new InputStreamReader(s.getInputStream()));
-	  ByteArrayOutputStream out = new ByteArrayOutputStream();
+	  // Create file to save the image (inlcuding a relative path)
+	  File newFile = new File("c:/Users/robin/eclipse-workspace/Computer Networks/src" + imagelocation);
+	  newFile.getParentFile().mkdirs();
+	  newFile.createNewFile();
+	  final FileOutputStream imageFile= new FileOutputStream("c:/Users/robin/eclipse-workspace/Computer Networks/src" + imagelocation);
+	  
+	  
+	  
 	  try { 
 		  s.setSoTimeout(5000);
-		  String output;
-		   boolean header = true;
-			  while (((output = in.readLine())!=null)&(header=true)) {
-				//  System.out.println(output);
-				  
-					  if (output.isEmpty()) {
-						  header = false;
-					  }
-			  } // Skipped the header
+		  BufferedInputStream in = new BufferedInputStream(s.getInputStream());
+		  boolean header = true;
+		  byte[] readBytes = new byte[2048];
+		  // ASSUMPTION: HEADER WILL NOT BE LONGER THEN 2048 ( or at least the New line empty line mark will not be broken in 2 by the 2048 mark)
+		  int counter;
+		  while ((counter = in.read(readBytes))!= -1) {
+				if (!header) { // Means header is ended
+					imageFile.write(readBytes,0,counter); // write all read bytes to file
+					
+				}
+				else
+					for (int i =0; i < 2045; i++) {
+						if (readBytes[i]==13 && readBytes[i+1]==10 && readBytes[i+2]==13 && readBytes[i+3]==10   ) {
+							header = false; // found end of header
+							System.out.println("Header Parsed");
+							imageFile.write(readBytes,i+4,2048-i-4);
+							break;
+						}
+					}
+		  }
 			
-			  while ()
-			int n = 0;
-			while (-1!=(n=in.read())) {
-			 out.write(buf,0,n);
 					 
 	  }
-	  }
+	  
 	  catch (Exception e) {
 		  System.out.println("Time Out");
 	  }
 	  //printing result from response
-	  System.out.println("result:");
-	  System.out.println(response.toString());
-	  String str = response.toString();
-	  FileWriter myWriter = new FileWriter("/Users/robin/eclipse-workspace/Computer Networks/src/" + url + ".HTML");
-	  
-	  //schrijft heel de website naar een file
-	  myWriter.write(str);
-	  myWriter.close();
 	  //gaat opzoek naar image
 	  
-	  in.close();
+	  
 	  dout.close();  
 		s.close();  
+		imageFile.close();
 		System.out.println("closed");
 		System.out.println("GET IS DONE");
 	}
 
- private void findingimage(String fileName) throws Exception {
+ private void findingimage(String fileName , String serverName) throws Exception {
 	 File input = new File(fileName);
-	 Document doc = Jsoup.parse(input,"UTF-8");
+	 Document doc = Jsoup.parse(input,"UTF-8",serverName);
 	 Elements img = doc.getElementsByTag("img");
 	 for (Element image : img) {
-		 String src = image.absUrl("src");
+		 String src = image.attr("src");
 		 System.out.println("Image found");
 		 System.out.println("src is;" + src);
-		 getImage(src);
+		 sendingGetRequestforImage(serverName, src);
 	 }
  }
  private void getImage(String src) {
